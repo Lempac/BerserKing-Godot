b0f97f519d2f55943f8ab165f382851c30c2d95c
22DP3APaza <103174656+22DP3APaza@users.noreply.github.com>
2023-10-25 13:12:00 +0300

    Made hiararchy for player and worm

--- Assets/Scripts/Game.gd
+++ Assets/Scripts/Game.gd
@@ -4,10 +4,11 @@ extends Node
 
 func _ready():
 	var levelData : Camera2D = load(Global.CurrentLevel).instantiate()
-	var player : Area2D = load(Global.Entity).instantiate()
+	#var player : Area2D = load(Global.Entity).instantiate()
 	#player.get_node("CollisionShape2D").collider_shape = PlayerData.shape
-	player.set_script(PlayerData.logic)
-	self.add_child(player)
+	#player.set_script(PlayerData.logic)
+	#self.add_child(player)
 	self.add_child(levelData.get_child(0).duplicate())
-	var cam = Camera2D.new()
-	player.add_child(cam)
+	#var cam = Camera2D.new()
+	#player.add_child(cam)
+	
