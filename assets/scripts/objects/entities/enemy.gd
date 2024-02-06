61b2ed694471ecfabef3121233ae3c07d1f12fc6
22DP3BBiks <13DPBBiksis@rvt.lv>
2024-02-06 19:26:24 +0200

    more enemies and new level

--- assets/scripts/objects/entities/enemy.gd
+++ assets/scripts/objects/entities/enemy.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @export var health : int
 @export var damage : int
 
+func _ready() -> void:
+	$AnimatedSprite2D.play("run")
 
 func _process(delta: float) -> void:
 	velocity = (Global.CurrentPlayer.position - position).normalized() * speed
@@ -13,6 +15,9 @@ func _process(delta: float) -> void:
 			var other = last_touch.get_collider()
 			if other.name == "Player" and other.health:
 				other.health -= damage
+				
+	$AnimatedSprite2D.flip_h = velocity.x < 0
+	
 	if health <= 0:
 		Global.CurrentGameKillCount += 1
 		queue_free()
--- assets/scripts/scenes/game.gd
+++ assets/scripts/scenes/game.gd
@@ -3,14 +3,29 @@ extends Node
 @export var spawn_rate : float
 @export var spawn_offset : Vector2i
 
-var worm = load("res://assets/objects/entities/worm.tscn")
+var enemies_path = "res://assets/objects/entities/enemies/"
+var enemies = []
+
+func load_enemies():
+	var enemies = []
+	var dir = DirAccess.open(enemies_path)
+	dir.list_dir_begin()
+	while true:
+		var file_name = dir.get_next()
+		if file_name == "":
+			break
+		elif !file_name.begins_with("."):
+			enemies.append(load(enemies_path + "/" + file_name).instantiate())
+	dir.list_dir_end()
+	return enemies
 
 func _ready():
 	add_child(Global.CurrentLevel)
+	enemies = load_enemies()
 	$SpawnTimer.wait_time = spawn_rate
 	
 func spawn():
-	var new_enemy : CharacterBody2D = worm.instantiate()
+	var new_enemy = enemies.pick_random().duplicate()
 	var size = DisplayServer.window_get_size() / 2 + spawn_offset
 	var player_pos = Global.CurrentPlayer.position
 	var new_pos = Vector2(randi_range(player_pos.x - size.x, player_pos.x + size.x), randi_range(player_pos.y - size.y, player_pos.y + size.y))
