1f7ddfc3f9d4b7e723793fcbafe96180698f5cdc
22DP3BBiks <13DPBBiksis@rvt.lv>
2024-02-23 20:51:01 +0200

    mirror test

--- assets/scripts/objects/mirror.gd
+++ assets/scripts/objects/mirror.gd
@@ -1,15 +1,8 @@
-extends Panel
+extends SubViewportContainer
 
 @onready var player := $"../Player"
-@onready var mirror_player := $SubViewportContainer/SubViewport/MirrorPlayer
-var pos
-
-func _ready() -> void:
-	pos = global_position
-	pos.y += size.y - 30
-	pos.x += size.x/2
+@onready var mirror_player := $SubViewport/MirrorPlayer
 
 func _process(delta: float) -> void:
-	$SubViewportContainer/SubViewport/Camera2D2.global_position = pos
 	mirror_player.flip_h = !player.get_node('./AnimatedSprite2D').flip_h
 	mirror_player.global_position = player.global_position
